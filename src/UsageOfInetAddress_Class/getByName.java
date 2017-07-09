a9558278344cf108196db4199b1edd9fe2033c7b
Chanaka1215 <nuwan.c.fernando@gmail.com>
2017-07-09 21:07:23 +0530

    finished static method getByName() of InetAddress class

---
+++ src/UsageOfInetAddress_Class/getByName.java
@@ -0,0 +1,20 @@
+package UsageOfInetAddress_Class;
+
+import com.sun.org.apache.xpath.internal.SourceTree;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+/**
+ * Created by Chanaka Fernando on 7/9/2017.
+ */
+public class getByName {
+    public static void main(String[] ar){
+        try {
+            InetAddress address = InetAddress.getByName("www.google.com");
+            System.out.println(address.getHostName()+" - "+address.getHostAddress());
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+    }
+}
